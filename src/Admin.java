5c40919b2ef608e95bff69dc5a7615a42352aca3
pdhan780 <100167525+pdhan780@users.noreply.github.com>
2022-03-25 17:04:15 -0600

    Add files via upload

---
+++ src/Admin.java
@@ -0,0 +1,29 @@
+
+public class Admin {
+
+    private int adminID;
+    private String adminPassword;
+
+    public int getAdminID() {
+        return adminID;
+    }
+
+    public void setAdminID(int adminID) {
+        this.adminID = adminID;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+
+
+
+
+
+
+}
---
+++ src/Student.java
@@ -0,0 +1,39 @@
+
+public class Student {
+
+    private int gpa;
+    private String major;
+    private int studentID;
+    private String studentPassword;
+
+
+
+    public int getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(int gpa) {
+        this.gpa = gpa;
+
+    }
+    public String getMajor() {
+        return major;
+    }
+    public void setMajor(String major) {
+        this.major = major;
+    }
+    public int getStudentID() {
+        return studentID;
+    }
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+    public String getPassword() {
+        return studentPassword;
+    }
+    public void setPassword(String password) {
+        this.studentPassword = password;
+    }
+
+
+}
