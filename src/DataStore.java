ca9cf868f6577e7bd7bae948c66596a8d11bf6b3
pdhan780 <100167525+pdhan780@users.noreply.github.com>
2022-03-25 17:05:31 -0600

    Update DataStore.java

--- src/DataStore.java
+++ src/DataStore.java
@@ -1,4 +1,9 @@
+import java.util.HashMap;
 
 public class DataStore {
+  
+    HashMap<Integer, Student> studentStorage = new HashMap<Integer, Student>();
+    
+    HashMap<Integer, Admin> adminStorage = new HashMap<Integer, Admin>();
 
 }
