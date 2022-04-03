861d4eb7c0109cd93241a7372203545cacdb1297
NB00001100 <100167954+NB00001100@users.noreply.github.com>
2022-04-02 20:41:33 -0600

    Update Student.java

--- src/main/Student.java
+++ src/main/Student.java
@@ -1,11 +1,18 @@
-package main;
+// author: Nimrat Brar, add names
 
 public class Student {
 
+
     private int gpa;
+    private String name;
     private String major;
     private int studentID;
     private String studentPassword;
+    private Fees [] outstandingFees = new Fees[50];
+    private Fees [] paidFees = new Fees[50];
+    private Course [] currentCourses = new Course[5]; // when we start adding to the courses or fees using our read, we first need to initialize all elements to null
+    private Course [] pastCourses = new Course[40];  // this is important for user interface otherwise it messes up printing the students info for past/current classes taken and fees.
+     // they can only take 5 courses currently and 40 courses in their entire academic career. if you want to change that change the size of them.
 
 
 
@@ -35,6 +42,48 @@ public class Student {
     public void setPassword(String password) {
         this.studentPassword = password;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Course [] getCurrentCourses() {
+        return currentCourses;
+    }
+
+    public void setCurrentCourses(Course [] currentCourses) {
+        this.currentCourses = currentCourses;
+    }
+
+    public Course [] getPastCourses() {
+        return pastCourses;
+    }
+
+    public void setPastCourses(Course [] pastCourses) {
+        this.pastCourses = pastCourses;
+    }
+
+    public Fees [] getOutstandingFees() {
+        return outstandingFees;
+    }
+
+    public void setOutstandingFees(Fees [] outstandingFees) {
+        this.outstandingFees = outstandingFees;
+    }
+
+    public Fees [] getPaidFees() {
+        return paidFees;
+    }
+
+    public void setPaidFees(Fees [] paidFees) {
+        this.paidFees = paidFees;
+    }
+
+
 
 
 }
