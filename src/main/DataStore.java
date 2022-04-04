ef596cc92bed1cce5a9bf793887337697de466d4
NB00001100 <100167954+NB00001100@users.noreply.github.com>
2022-04-04 12:27:01 -0600

    Update DataStore.java

--- src/main/DataStore.java
+++ src/main/DataStore.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 
 public class DataStore {
   
+  
     HashMap<Integer, Student> studentStorage = new HashMap<Integer, Student>();
     
     HashMap<Integer, Admin> adminStorage = new HashMap<Integer, Admin>();
