e4ace090e58321887fb742e64db988c505e653c8
KushalS02 <102070113+KushalS02@users.noreply.github.com>
2022-04-05 16:23:29 -0600

    Update ExportConnector.java

--- src/main/ExportConnector.java
+++ src/main/ExportConnector.java
@@ -1,9 +1,30 @@
 package main;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ExportConnector {
 
-    public static Boolean uploadToGithub (String contents, String filename) {
-        return true;
-    }
+    public static Boolean uploadToGithub (String contents, String fileAddress) {
 
+        try {
+
+            File myObj = new File(fileAddress);
+            myObj.delete();
+            myObj.createNewFile();
+
+            FileWriter myWriter = new FileWriter(myObj);
+            myWriter.write(contents);
+            myWriter.close();
+
+            return true;
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+
+            return false;
+        }
+    }
 }
