41742c035a7e96f947426e842725a36562a4ea38
NB00001100 <100167954+NB00001100@users.noreply.github.com>
2022-04-05 12:27:29 -0600

    Update AdminInterface.java

--- src/main/AdminInterface.java
+++ src/main/AdminInterface.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 
 public class AdminInterface {
 
-
-
     public static boolean Admin_interface() {
 
         PrintStream output = new PrintStream(System.out);
@@ -16,52 +14,34 @@ public class AdminInterface {
 
         options_value = input.nextInt();
 
-        if(options_value == 1)
-        {
-            AdminActions.editMajor(output,input);
+        if (options_value == 1) {
+            AdminActions.editMajor(output, input);
 
-
-        }
-        else if(options_value == 2)
-        {
+        } else if (options_value == 2) {
             AdminActions.editCurrentCourses(output, input);
-        }
-        else if(options_value == 3)
-        {
-            AdminActions.editPreviousCourses(output,input);
-        }
-        else if(options_value == 4)
-        {
-            AdminActions.editGpa(output,input);
+        } else if (options_value == 3) {
+            AdminActions.editPreviousCourses(output, input);
+        } else if (options_value == 4) {
+            AdminActions.editGpa(output, input);
 
-        }
-        else if(options_value == 5)
-        {
-            AdminActions.editOutstandingFees(output,input);
-        }
-        else if(options_value == 6)
-        {
-             AdminActions.editFeesPaid(output,input);
-        }
-        else if(options_value == 7)
-        {
-            AdminActions.editStudentId(output,input);
+        } else if (options_value == 5) {
+            AdminActions.editOutstandingFees(output, input);
+        } else if (options_value == 6) {
+            AdminActions.editFeesPaid(output, input);
+        } else if (options_value == 7) {
+            AdminActions.editStudentId(output, input);
 
-        }
-        else if(options_value == 8)
-        {
-            AdminActions.editStudentPassword(output,input);
-        }
-        else if(options_value == 9)
-        {
-            AdminActions.addStudent(output,input);
-        }
-        else if(options_value == 10)
-        {
-            AdminActions.removeStudent(output,input);
-        }
-        else if(options_value == 12)
-        {
+        } else if (options_value == 8) {
+            AdminActions.editStudentPassword(output, input);
+        } else if (options_value == 9) {
+            AdminActions.addStudent(output, input);
+        } else if (options_value == 10) {
+            AdminActions.removeStudent(output, input);
+        } else if (options_value == 11) {
+
+            AdminActions.view_student_info(output, input);
+
+        } else if (options_value == 12) {
             output.close();
             input.close();
             return false;
@@ -74,9 +54,7 @@ public class AdminInterface {
 
     }
 
-
-    public static  void show_admin_options(PrintStream output)
-    {
+    public static void show_admin_options(PrintStream output) {
         output.println();
         output.println();
         output.println(" Welcome to The administration interface ");
@@ -92,16 +70,12 @@ public class AdminInterface {
         output.println(" To edit student password enter 8 :");
         output.println(" To add a student to the Student Database System enter: 9");
         output.println(" To remove a student from the Student DataBase System enter: 10 ");
+        output.println(" To view a Student's information enter: 11");
         output.println(" To quit enter: 12");
         output.println();
         output.print(" Enter here: ");
         return;
 
     }
-
-
-
-
-
 
 }
