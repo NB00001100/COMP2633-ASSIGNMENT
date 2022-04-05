aac8044280ab4832417e579877a5ecc3c330542d
NB00001100 <100167954+NB00001100@users.noreply.github.com>
2022-04-05 12:29:31 -0600

    Update AdminActions.java

--- src/main/AdminActions.java
+++ src/main/AdminActions.java
@@ -6,12 +6,10 @@ public class AdminActions {
     private static int student_id = 0;
     private static String name = "";
 
-
-    public static void editMajor(PrintStream output, Scanner input)
-    {
+    public static void editMajor(PrintStream output, Scanner input) {
         String major = null;
         Student majorChanged = null;
-        enterStudentInfo(output,input);
+        enterStudentInfo(output, input);
         output.println();
         output.println();
         output.println(" Enter the change in major ");
@@ -23,50 +21,85 @@ public class AdminActions {
 
     }
 
-    public static void editCurrentCourses(PrintStream output, Scanner input)
-    {
-          // remove from array and shift
-
-    }
-
-    public static void editPreviousCourses(PrintStream output, Scanner input)
-    {
-          // remove from array and shift
-
-    }
-    public static void editGpa(PrintStream output, Scanner input)
-    {
-        
-
-    }
-    public static void editOutstandingFees(PrintStream output, Scanner input)
-    {
-          // remove from array and shift
-
-    }
-    public static void editFeesPaid(PrintStream output, Scanner input)
-    {
+    public static void editCurrentCourses(PrintStream output, Scanner input) {
         // remove from array and shift
- 
-    }
-    public static void editStudentId(PrintStream output, Scanner input)
-    {
 
     }
-    public static void editStudentPassword(PrintStream output, Scanner input)
-    {
+
+    public static void editPreviousCourses(PrintStream output, Scanner input) {
+        // remove from array and shift
 
     }
-    public static void addStudent(PrintStream output, Scanner input)
-    {
+
+    public static void editGpa(PrintStream output, Scanner input) {
+        float new_gpa = 0;
+        Student gpaChanged = null;
+        enterStudentInfo(output, input);
+        output.println();
+        output.println();
+        output.println(" Enter change in GPA");
+        output.print(" New GPA: ");
+        new_gpa = input.nextFloat();
+        gpaChanged = DataStore.findStudent(student_id, name);
+        gpaChanged.setGpa(new_gpa);
+        DataStore.getStudentStorage().replace(student_id, gpaChanged);
 
     }
-    public static void removeStudent(PrintStream output, Scanner input)
-    {
+
+    public static void editOutstandingFees(PrintStream output, Scanner input) {
+        // remove from array and shift
 
     }
-    public static void enterStudentInfo(PrintStream output, Scanner input)
-    {
+
+    public static void editFeesPaid(PrintStream output, Scanner input) {
+        // remove from array and shift
+
+    }
+
+    public static void editStudentId(PrintStream output, Scanner input) {
+
+        int new_studentId = 0;
+        Student IdChanged = null;
+        enterStudentInfo(output, input);
+        output.println();
+        output.println();
+        output.println(" Enter change in student ID");
+        output.print(" New Student ID: ");
+        new_studentId = input.nextInt();
+        IdChanged = DataStore.findStudent(new_studentId, name);
+        IdChanged.setStudentID(new_studentId);
+        DataStore.getStudentStorage().replace(student_id, IdChanged);
+
+    }
+
+    public static void editStudentPassword(PrintStream output, Scanner input) {
+        String new_password = null;
+        Student passwordChanged = null;
+        enterStudentInfo(output, input);
+        output.println();
+        output.println();
+        output.println(" Enter change in pasword");
+        output.print(" New Password: ");
+        new_password = input.nextLine();
+        passwordChanged = DataStore.findStudent(student_id, name);
+        passwordChanged.setPassword(new_password);
+        DataStore.getStudentStorage().replace(student_id, passwordChanged);
+    }
+
+    public static void addStudent(PrintStream output, Scanner input) {
+
+    }
+
+    public static void removeStudent(PrintStream output, Scanner input) {
+
+    }
+
+    public static void view_student_info(PrintStream output, Scanner input) {
+        enterStudentInfo(output, input);
+        StudentInterface.student_interface(student_id);
+    }
+
+    public static void enterStudentInfo(PrintStream output, Scanner input) {
         output.println();
         output.println();
         output.println(" Enter the KPN University ID and Student's name");
@@ -92,8 +125,5 @@ public class AdminActions {
     public static void setName(String name) {
         AdminActions.name = name;
     }
-
-
-
 
 }
