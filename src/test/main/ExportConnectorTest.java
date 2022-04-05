1628718611d7f19806c83e508b96f53073b19905
KushalS02 <102070113+KushalS02@users.noreply.github.com>
2022-04-05 16:31:23 -0600

    ;-;

---
+++ src/test/main/ExportConnectorTest.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+
+class ExportConnectorTest {
+
+    @Test
+    void testUploadToGithub() {
+
+        ExportConnector.uploadToGithub("testijgn", "C:\\Users\\kusha\\OneDrive\\Documents\\GitHub\\COMP2633-ASSIGNMENT\\res\\testold.txt");
+
+    }
+
+}
